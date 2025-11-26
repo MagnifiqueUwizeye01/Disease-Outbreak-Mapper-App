c22102168c67630c317813e74e26a207a2708cbd
princelouissevelin <princelouissevelin@gmail.com>
2025-11-26 04:04:14 +0200

    Create basic Android app structure
    - Add AndroidManifest.xml with main activity declaration
    - Create MainActivity.java as entry point
    - Add build.gradle with dependencies for History and Profile screens
    - Set up foundation for CHW disease reporting features

---
+++ app/src/main/java/com/healthmapper/chwapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.healthmapper.chwapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Temporary main activity - we'll add proper UI later
+        // This is just to satisfy the AndroidManifest reference
+    }
+}
