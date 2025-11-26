e63476e2b5d89f2c291995c22881ab093dec5107
Igizeneza Serge Benit <hacksergeb@gmail.com>
2025-11-27 00:16:03 +0200

    activities screen(need polish)

---
+++ app/src/main/java/com/healthtracker/chw/fragments/DashboardFragment.java
@@ -0,0 +1,76 @@
+package com.healthtracker.chw.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.healthtracker.chw.R;
+
+public class DashboardFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        // Quick action chips
+        View chipReportNow = view.findViewById(R.id.chip_report_now);
+        View chipHighRisk = view.findViewById(R.id.chip_high_risk);
+        View chipSyncPending = view.findViewById(R.id.chip_sync_pending);
+
+        if (chipReportNow != null) {
+            chipReportNow.setOnClickListener(v ->
+                    navController.navigate(R.id.reportCaseFragment));
+        }
+
+        if (chipHighRisk != null) {
+            chipHighRisk.setOnClickListener(v ->
+                    navController.navigate(R.id.mapFragment));
+        }
+
+        if (chipSyncPending != null) {
+            chipSyncPending.setOnClickListener(v ->
+                    navController.navigate(R.id.offlinePendingCasesFragment));
+        }
+
+        // Main cards
+        View cardReportCase = view.findViewById(R.id.card_report_case);
+        View cardOutbreakMap = view.findViewById(R.id.card_outbreak_map);
+        View cardTrends = view.findViewById(R.id.card_trends);
+        View cardHistory = view.findViewById(R.id.card_history);
+
+        if (cardReportCase != null) {
+            cardReportCase.setOnClickListener(v ->
+                    navController.navigate(R.id.reportCaseFragment));
+        }
+
+        if (cardOutbreakMap != null) {
+            cardOutbreakMap.setOnClickListener(v ->
+                    navController.navigate(R.id.mapFragment));
+        }
+
+        if (cardTrends != null) {
+            cardTrends.setOnClickListener(v ->
+                    navController.navigate(R.id.analyticsFragment));
+        }
+
+        if (cardHistory != null) {
+            cardHistory.setOnClickListener(v ->
+                    navController.navigate(R.id.caseHistoryFragment));
+        }
+    }
+}
