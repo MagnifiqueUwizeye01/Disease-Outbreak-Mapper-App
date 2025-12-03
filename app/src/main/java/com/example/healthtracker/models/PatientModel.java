8cac959840a7789e0b68dcea3df0791ed9fafe97
Raissa427 <numubyeyiirumvaraissa@gmail.com>
2025-12-03 04:15:06 +0200

    Add PatientModel.java: data model for patient details

---
+++ app/src/main/java/com/example/healthtracker/models/PatientModel.java
@@ -0,0 +1,10 @@
+package com.example.healthtracker.models;
+
+
+public class PatientModel {
+    public String id;
+    public String name;
+    public int age;
+    public String gender;
+    public String identifier; // optional
+}
